9029b2ca6875e8dc600f63e949af03ab223d276e
Subhamay Bhattacharyya <subhamay.aws+github+cloudworks@gmail.com>
2025-04-03 22:11:57 -0400

    SB-0001: Enhance action.yaml and main.js to support template path and improve parameter handling

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
-const core = require('@actions/core');
+const core = require("@actions/core");
 
+function run() {
+  try {
+    const configFile = core.getInput("config-file");
+    const environment = core.getInput("environment");
 
-function run(){
-    core.notice('Hello from my custom JavaScript action!');
+    const configData = JSON.parse(fs.readFileSync(configFile, "utf8"));
+
+    const envParamsArray = configData.parameters[environment];
+    const projectName = configData["project-name"];
+    const stackPrefix = configData["stack-prefix"];
+    const stackSuffix = configData["stack-suffix"];
+    const templatePath = configData["template-path"];
+    const randomString = Math.random().toString(36).substring(2, 7); // Generate random string
+    const stackName = `${projectName}-${stackPrefix}-${stackSuffix}-${randomString}`;
+    console.log("Stack Name: " + stackName);
+    console.log("Template Path: " + templatePath);
+    console.log("Environment: " + environment);
+    console.log("Random String: " + randomString);
+
+    if (!envParamsArray || !Array.isArray(envParamsArray)) {
+      throw new Error(`No parameters found for environment: ${environment}`);
+    }
+
+    // Flatten all objects in the array (support for multiple maps if needed)
+    const flatParams = envParamsArray.reduce(
+      (acc, obj) => ({ ...acc, ...obj }),
+      {}
+    );
+
+    const formattedParams = Object.entries(flatParams)
+      .map(([key, value]) => `ParameterKey=${key},ParameterValue=${value}`)
+      .join("\n");
+
+    console.log("Generated Parameters:\n" + formattedParams);
+
+    core.setOutput("parameters", formattedParams);
+    core.setOutput("stack-name", stackName);
+    core.setOutput("template-path", templatePath);
+    core.setOutput("ci-build-identifier", randomString);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-run ();
+run();
